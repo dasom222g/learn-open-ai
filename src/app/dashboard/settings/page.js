4318d279712c04f92e1083dcaa7bf404a7fdc894
dasom222g <dasom228@gmail.com>
2023-07-17 22:17:22 +0900

    Set first style

---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,12 @@
+const Settings = () => {
+  return (
+    <div>
+      <code cla ssName="font-mono font-bold">
+        src/app/dashboard/settings.js
+      </code>
+      ㅁㅓ야머야!!
+    </div>
+  );
+};
+
+export default Settings;
