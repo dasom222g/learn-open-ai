9b64ec8d54eb76150036a67fbf9499758ef29aed
dasom222g <dasom228@gmail.com>
2023-07-21 13:22:36 +0900

    Add form

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,12 @@ export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main>
-      <h1 className="text-red-500">Hello world</h1>
+      <img src="./favicon.ico" />
+      <h3>Generate animal name</h3>
+      <form action="/post">
+        <input type="text" />
+        <button type="submit">제출</button>
+      </form>
     </main>
   );
 }
