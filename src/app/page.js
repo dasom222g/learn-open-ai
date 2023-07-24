a4f8ba92181c140be4783fc4b95fa82c2f6be96f
dasom222g <dasom228@gmail.com>
2023-07-24 22:23:03 +0900

    Use useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
-// import './globals.css';
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+  };
+
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main>
@@ -9,6 +16,10 @@ export default function Home() {
         <input type="text" />
         <button type="submit">Submit</button>
       </form>
+      <p>counter {counter}</p>
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Button
+      </button>
     </main>
   );
 }
