f4149114e5cf8938871d0c2e97c98dbe14c30b7d
dasom222g <dasom228@gmail.com>
2023-07-26 00:06:05 +0900

    Set input

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,14 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log('value', value);
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
   return (
@@ -12,14 +17,14 @@ export default function Home() {
     <main>
       <img src="./favicon.ico" />
       <h3>Generate animal name</h3>
-      <form action="/post">
-        <input type="text" />
+      <form action="/post" onSubmit={(event) => handleSubmit(event)}>
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => handleChange(event)}
+        />
         <button type="submit">Submit</button>
       </form>
-      <p>counter {counter}</p>
-      <button type="button" onClick={(event) => handleSubmit(event)}>
-        Button
-      </button>
     </main>
   );
 }
