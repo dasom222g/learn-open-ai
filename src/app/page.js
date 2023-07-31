234b9ed9b80dc6b1197e06cda7651f31b4f205f9
dasom222g <dasom228@gmail.com>
2023-07-31 22:16:48 +0900

    Add style

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import styles from './index.module.css';
 
 export default function Home() {
   const [value, setValue] = useState('');
@@ -12,12 +13,17 @@ export default function Home() {
 
   const handleChange = (event) => {
     setValue(event.target.value);
+    console.log('styles', styles.main);
   };
+
+  // useEffect(() => {
+  //   console.log('styles', styles.main);
+  // });
 
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main>
-      <img src="./favicon.ico" />
+    <main className={styles.main}>
+      <img src="./favicon.ico" className={styles.icon} />
       <h3>Generate animal name</h3>
       <form action="/post" onSubmit={(event) => handleSubmit(event)}>
         <input
@@ -27,6 +33,7 @@ export default function Home() {
         />
         <button type="submit">Submit</button>
       </form>
+      <div className={styles.result}>결과 부분</div>
     </main>
   );
 }
