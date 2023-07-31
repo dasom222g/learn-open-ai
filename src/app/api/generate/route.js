d28359414ca834397a4db8e879cf17425cf4db43
dasom222g <dasom228@gmail.com>
2023-08-01 00:08:43 +0900

    Add post api

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -9,11 +9,22 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-console.log('env', process.env.OPENAI_API_KEY);
+export async function POST(request) {
+  const body = await request.json();
+  const animal = body.animal || '';
+  // const completion = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   // 메시지 설정 | 메커니즘은 system -> user -> assistant -> user -> assistant 반복
+  //   prompt: `Suggest three pet names for the follow`,
+  // });
+  // const fortune = completion.data.choices[0].message;
+  // console.log('🚀 : fortune===>', fortune);
+  return Response.json({ result: 'bla' });
+}
 
-const response = await openai.createChatCompletion({
-  model: 'gpt-3.5-turbo',
-  messages: [],
-  temperature: 0.8,
-  max_tokens: 256,
-});
+// const response = await openai.createChatCompletion({
+//   model: 'gpt-3.5-turbo',
+//   messages: [],
+//   temperature: 0.8,
+//   max_tokens: 100,
+// });
